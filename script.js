01288e9faf302748b2d8c0fd8f2d4f0ef5ec68c1
“Oscar” <os0c40raa@gmail.com>
2023-02-10 17:52:39 -0600

    Revert "Se agrego reaparicion de la animacion primera version, activacion cada 4 minutos."
    This reverts commit 1c8170a3a3cbbf3304fb6fc2003e9d13bf0b199b.

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function btnEncriptar(){
     mensaje.value = textoEncriptado;
     textArea.value = "";
     mensaje.style.backgroundImage = "none";
-    animacionExplicacion.cancel;
+    tutorial.style.animationPlayState = "paused";
     tutorial.style.visibility = "hidden";
     copiar.style.visibility = "visible";
 }
@@ -25,11 +25,10 @@ function btnDesencriptar(){
     const textoEncriptado = desencriptar(textArea.value);
     mensaje.value = textoEncriptado;
     textArea.value = "";
-    animacionExplicacion.cancel;
+    tutorial.style.animationPlayState = "paused";
     tutorial.style.visibility = "hidden";
     copiar.style.visibility = "visible";
 }
-
 
 function encriptar(stringEncriptado){
     let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
@@ -55,21 +54,11 @@ function desencriptar(stringDesencriptado){
     return stringDesencriptado;
 }
 
-var animacionExplicacion = [
-    {transform: "rotateX(10deg) scaleX(1.06) scaleY(0.89)", top: "490px", right: "62px"},
-    {transform: "rotateY(8deg) scaleX(0.92)  scaleY(1.04)", top: "540px", right: "74px"},
-    {transform: "rotateX(28deg)  scaleX(1.06)  scaleY(0.91)", top: "500px", right: "87px"},
-    {transform: "rotateX(4deg)   scaleX(0.91)   scaleY(1.05)", top: "550px", right: "104px"},
-    {transform: "rotateY(25deg) scaleX(1.05)  scaleY(0.87)", top: "500px", right: "85px"}, 
-    {transform: "rotateX(4deg) scaleX(0.9)  scaleY(1.06)", top: "550px", right: "68px"}]
-
-function animacionTutorial(){
-    tutorial.animate(animacionExplicacion, { id: "bounce", delay: -1000, iterations: Infinity, duration: 16000, direction: "alternate"})
+function cambioDeAnimacion(){
+    tutorial.style.animation = "animacion-explicacion 16s linear -0.09s infinite alternate";
 }
+tutorial.addEventListener("animationend", cambioDeAnimacion);
 
-tutorial.addEventListener("animationend", animacionTutorial);
-
-animacionTimer = setInterval(animacionTutorial, 240000);
 
 copiar.onclick = function(){
     let texto = mensaje.value;
@@ -81,13 +70,7 @@ copiar.onclick = function(){
         alert("Error in copying text: ", err)
     })
 }
-
-
-
-
 /* 
-Version alterna de copiar texto.
-
 function myFunction() {
    
      // Copy the text inside the text field
